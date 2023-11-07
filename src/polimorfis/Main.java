fe4529b514c95b8534671c31705ffd366ca65cc0
GRENDLEARD <grendleard@gmail.com>
2023-11-07 02:49:15 +0100

    POO

---
+++ src/polimorfis/Main.java
@@ -0,0 +1,12 @@
+package polimorfis;
+public class Main {
+    public static void main(String[] args) {
+        Animal miMascota;
+
+        miMascota = new Perro();
+        miMascota.hacerSonido();  // Llama al método hacerSonido de la clase Perro
+
+        miMascota = new Gato();
+        miMascota.hacerSonido();  // Llama al método hacerSonido de la clase Gato
+    }
+}
---
+++ src/polimorfis/animal.java
@@ -0,0 +1,23 @@
+
+package polimorfis;
+
+class Animal {
+    public void hacerSonido() {
+        System.out.println("Este es un animal genérico");
+    }
+}
+
+class Perro extends Animal {
+    @Override
+    public void hacerSonido() {
+        System.out.println("El perro ladra");
+    }
+}
+
+class Gato extends Animal {
+    @Override
+    public void hacerSonido() {
+        System.out.println("El gato maulla");
+    }
+}
+
